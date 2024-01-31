fb296cd54c24fc9cf652def56c6990bdd90cbd26
Dee Williams <deejmwilliams@gmail.com>
2024-01-31 14:33:43 -0500

    Add test data and skeleton of exam display table

--- client/src/app/admin/page.jsx
+++ client/src/app/admin/page.jsx
@@ -1,12 +1,66 @@
 import { Button } from "@/components/ui/button"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+import TestData from "../testdata.json"
 
-export default function Admin()
-{
+// Can we assume exams conform to correct data types due to schema checks in back end?
+const ExamTable = ({exams}) => {
+    return(
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Patient ID</TableHead>
+                    <TableHead>Exam ID</TableHead>
+                    <TableHead>Image</TableHead>
+                    <TableHead>Key Findings</TableHead>
+                    <TableHead>Brixia Score</TableHead>
+                    <TableHead>Age</TableHead>
+                    <TableHead>Sex</TableHead>
+                    <TableHead>BMI</TableHead>
+                    <TableHead>ZIP Code</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {exams.map((exam) => 
+                    {
+                        return(
+                            exam != null ? 
+                            <TableRow>
+                                <TableCell>{exam["patientId"]}</TableCell>
+                                <TableCell>{exam.examId}</TableCell>
+                                <TableCell>{exam.imageURL}</TableCell>
+                                <TableCell>{exam.keyFindings}</TableCell>
+                                <TableCell>{exam.brixiaScores}</TableCell>
+                                <TableCell>{exam.age}</TableCell>
+                                <TableCell>{exam.sex}</TableCell>
+                                <TableCell>{exam.bmi}</TableCell>
+                                <TableCell>{exam.zipCode}</TableCell>
+                            </TableRow> :
+                            <></>)
+                        }
+                )
+                }
+            </TableBody>
+        </Table>
+    )
+}
+
+const Admin = () => {
     return (
         <div className="text-center font-bold py-5">
             <h1>This is the Admin Page</h1>
             <Button>Add new record</Button>
             <Button>Delete selected record(s)</Button>
+            <ExamTable exams={TestData["exams"]} />
         </div>
     )   
 }
+
+
+export default Admin;
