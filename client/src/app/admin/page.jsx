43c4db8ce5a9350a6b7973a36d15cfb75e208e1f
Dee Williams <deejmwilliams@gmail.com>
2024-01-31 13:50:22 -0500

    Add skeleton for edit buttons on admin page

--- client/src/app/admin/page.jsx
+++ client/src/app/admin/page.jsx
@@ -1,11 +1,12 @@
-
+import { Button } from "@/components/ui/button"
 
 export default function Admin()
 {
     return (
         <div className="text-center font-bold py-5">
             <h1>This is the Admin Page</h1>
-            <a href="/" className="inline-block p-3"> Go Back to home</a>
+            <Button>Add new record</Button>
+            <Button>Delete selected record(s)</Button>
         </div>
     )   
 }
