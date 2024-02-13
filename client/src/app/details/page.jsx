180fa1444824029a475444947c135870e6fd1136
angeliedarb <angeliedarbouze@gmail.com>
2024-02-13 18:45:37 -0500

    Added some of detail page

---
+++ client/src/app/details/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Detail()
+{
+    return (
+        <div className="text-center font-bold py-5">
+            <h1>This is the Detail Page</h1>
+            <div className="inline-block px-5 py-2">Patient info</div>
+            <div className="inline-block px-5 py-2">Exam info</div>
+            <div>Left column</div> 
+            <div>Right column</div>
+            <a href="/" className="inline-block p-3"> Go Back to home</a>
+        </div>
+    )   
+}
