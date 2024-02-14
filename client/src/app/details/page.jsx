97865a4671f92d27cd5a0c96da514dbd6b5bdd5a
angeliedarb <angeliedarbouze@gmail.com>
2024-02-13 19:26:06 -0500

    new code

--- client/src/app/details/page.jsx
+++ client/src/app/details/page.jsx
@@ -5,10 +5,28 @@ export default function Detail()
     return (
         <div className="text-center font-bold py-5">
             <h1>This is the Detail Page</h1>
+            <div className = "row">
             <div className="inline-block px-5 py-2">Patient info</div>
+            <div>
+                <ul>
+                    <li>Patient ID</li> <li>{exam.patientId}</li>
+                    <li>Age</li> <li>{exam.age}</li>
+                    <li>Sex</li> <li>{exam.sex}</li>
+                    <li>BMI</li> <li>{exam.bmi}</li>
+                    <li>Zip Code</li> <li>{exam.zipCdoe}</li>
+                </ul>
+            </div> 
             <div className="inline-block px-5 py-2">Exam info</div>
-            <div>Left column</div> 
-            <div>Right column</div>
+            <div>
+                <ul>
+                    <li>Exam ID</li> <li>{exam.examId}</li>
+                    <li>Image</li>  <li>{exam.imageURL}</li>
+                    <li>Date</li>  <li>{exam.date}</li>
+                    <li>Key Findings</li>  <li>{exam.keyFindings}</li>
+                    <li>Brixia Score</li>  <li>{exam.brixiaScores}</li>
+                </ul>
+            </div>
+            </div>
             <a href="/" className="inline-block p-3"> Go Back to home</a>
         </div>
     )   
