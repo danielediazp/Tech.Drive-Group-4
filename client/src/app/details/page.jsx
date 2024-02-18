7e4eb0abcb6f553f8fb69c0822d6c187e18a4dac
angeliedarb <angeliedarbouze@gmail.com>
2024-02-17 22:54:05 -0500

    Edited detail page

--- client/src/app/details/page.jsx
+++ client/src/app/details/page.jsx
@@ -1,33 +1,78 @@
 
+import { 
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
-export default function Detail()
-{
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+
+import TestData from "@/app/testdata.json"
+
+const MenuButton = () => {
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger><DotsVerticalIcon /></DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>Edit exam</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Delete exam</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+const ExamTable = ({exams}) => {
+    return(
+        
+    <Table>
+        <TableHead className="text-center font-bold py-10 text-lg">Patient Info</TableHead>
+        <TableHead className="text-center font-bold py-10 text-lg">Exam Info</TableHead>                
+       
+        <TableBody>
+         <TableRow>
+            <TableCell className="text-left font-bold py-5">Patient ID: {exams.patientId}</TableCell>
+            <TableCell className="text-left font-bold py-5">Exam ID: {exams.examId}</TableCell>
+        </TableRow>
+        <TableRow>
+            <TableCell className="text-left font-bold py-5">Age: {exams.age}</TableCell>
+            <TableCell className="text-left font-bold py-5">Image: {exams.imageURL}</TableCell>
+        </TableRow>     
+        <TableRow>
+            <TableCell className="text-left font-bold py-5">Sex: {exams.sex}</TableCell>
+            <TableCell className="text-left font-bold py-5">Date: {exams.date}</TableCell>
+        </TableRow>     
+        <TableRow>
+            <TableCell className="text-left font-bold py-5">BMI: {exams.bmi}</TableCell>
+            <TableCell className="text-left font-bold py-5">Key Findings: {exams.keyFindings}</TableCell>
+        </TableRow>     
+        <TableRow>
+            <TableCell className="text-left font-bold py-5">Zip Code: {exams.zipCode} </TableCell>
+            <TableCell className="text-left font-bold py-5">Brixia Score:{exams.brixiaScores} </TableCell>
+        </TableRow>  
+        </TableBody>
+    </Table>
+           
+    )
+}
+
+const Admin = () => {
     return (
-        <div className="text-center font-bold py-5">
-            <h1>This is the Detail Page</h1>
-            <div className = "row">
-            <div className="inline-block px-5 py-2">Patient info</div>
-            <div>
-                <ul>
-                    <li>Patient ID</li> <li>{exam.patientId}</li>
-                    <li>Age</li> <li>{exam.age}</li>
-                    <li>Sex</li> <li>{exam.sex}</li>
-                    <li>BMI</li> <li>{exam.bmi}</li>
-                    <li>Zip Code</li> <li>{exam.zipCdoe}</li>
-                </ul>
-            </div> 
-            <div className="inline-block px-5 py-2">Exam info</div>
-            <div>
-                <ul>
-                    <li>Exam ID</li> <li>{exam.examId}</li>
-                    <li>Image</li>  <li>{exam.imageURL}</li>
-                    <li>Date</li>  <li>{exam.date}</li>
-                    <li>Key Findings</li>  <li>{exam.keyFindings}</li>
-                    <li>Brixia Score</li>  <li>{exam.brixiaScores}</li>
-                </ul>
-            </div>
-            </div>
-            <a href="/" className="inline-block p-3"> Go Back to home</a>
+        <div >
+            
+            
+            <ExamTable exams={TestData["exams"]} />
         </div>
     )   
 }
+
+
+export default Admin;
